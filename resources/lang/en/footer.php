f9585055a8274c9d00f6f54ba4fa03f1d43d50d2
DESKTOP-RAAIMNI\ACER <exanp>
2022-09-29 07:43:04 +0700

    first commit

---
+++ resources/lang/en/footer.php
@@ -0,0 +1,14 @@
+<?php
+
+  return [
+    'AboutUs'          =>      'Contact Info',
+    'address'          =>      'No.18 Pho Vien, Duc Thang, Bac Tu Liem, Hanoi.',
+    'FollowUs'         =>      'Follow Us',
+    'visa'             =>      'Visa method',
+    'mastercard'       =>      'MasterCard method',
+    'jcb'              =>      'JCB method',
+    'cod'              =>      'Cash on Delivery - COD.',
+    'nganluong'        =>      'Provided by NganLuong.',
+    'payment'          =>      'Payment Method',
+    'support'          =>      'Customer Support',
+  ];
---
+++ resources/lang/vn/footer.php
@@ -0,0 +1,14 @@
+<?php
+
+  return [
+    'AboutUs'          =>      'Thông tin liên hệ',
+    'address'          =>      'Số 18 Phố Viên, Đức Thắng, Bắc Từ Liêm, Hà Nội.',
+    'FollowUs'         =>      'Theo Dõi Chúng Tôi',
+    'visa'             =>      'Thanh toán bằng thẻ Visa.',
+    'mastercard'       =>      'Thanh toán bằng thẻ MasterCard.',
+    'jcb'              =>      'Thanh toán bằng thẻ JCB.',
+    'cod'              =>      'Thanh toán khi nhận hàng.',
+    'nganluong'        =>      'Giải pháp thanh toán qua Ngân Lượng.',
+    'payment'          =>      'Hỗ Trợ Thanh Toán',
+    'support'          =>      'Hỗ Trợ Khách Hàng',
+  ];
